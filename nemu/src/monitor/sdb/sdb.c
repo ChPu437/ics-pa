8e15248ad33aa634c7336f9ba12732a62c6b4234
tracer-ics2022 <tracer@njuics.org>
2023-09-27 01:06:55 +0800

    >  compile NEMU 998244353 Jing Pu Linux jing-VirtualBox 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  01:06:55 up  1:44,  1 user,  load average: 0.06, 0.14, 0.08

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -150,7 +150,7 @@ static int cmd_x(char *args) {
 	printf("!!!%d %x\n", N, EXPR);
 
 	for (int i = 0; i < N; i++)
-		printf("0x%x: 0x%u\n", EXPR + 4 * i, paddr_read(EXPR, 4));
+		printf("0x%x: 0x%x\n", EXPR + 4 * i, paddr_read(EXPR, 4));
 	return 0;
 }
 
