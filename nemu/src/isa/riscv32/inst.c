043428c15475ebb321fa2bed3c4af663f9192bd7
tracer-ics2022 <tracer@njuics.org>
2023-11-01 00:42:56 +0800

    >  compile NEMU 998244353 Jing Pu Linux jing-VirtualBox 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:42:56 up 24 min,  1 user,  load average: 0.16, 0.12, 0.08

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -29,6 +29,7 @@ enum {
   // Extended types
   TYPE_J,
   TYPE_R,
+  TYPE_B,
 };
 
 #define src1R() do { *src1 = R(rs1); } while (0)
@@ -38,6 +39,7 @@ enum {
 #define immS() do { *imm = (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7); } while(0)
 // Extended immediate type
 #define immJ() do { *imm = ((SEXT(BITS(i, 31, 31), 1) << 19) | (BITS(i, 19, 12) << 11) | (BITS(i, 20, 20) << 10) | BITS(i, 30, 21)) << 1;} while(0)
+#define immB() do { *imm = ((SEXT(BITS(i, 31, 31), 1) << 11) | (BITS(i, 7, 7) << 10) | (BITS(i, 30, 25) << 4) | BITS(i, 11, 8)) << 1;} while(0)
 // // 左移给后续字节留下空间
 // // SEXT代表符号扩展
 
@@ -51,8 +53,10 @@ static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_
     case TYPE_U:                   immU(); break;
     case TYPE_S: src1R(); src2R(); immS(); break;
     // Extended type
+    // Notice that lowbit->highbit here, reverse from imm above
 	case TYPE_J:                   immJ(); break; 
 	case TYPE_R: src1R(); src2R();         break;
+	case TYPE_B: src1R(); src2R(); immB(); break;
   }
 }
 
@@ -87,7 +91,9 @@ static int decode_exec(Decode *s) {
   INSTPAT("0000000 ????? ????? 000 ????? 01100 11", add    , R, R(rd) = src1 + src2);
   INSTPAT("0100000 ????? ????? 000 ????? 01100 11", sub    , R, R(rd) = src1 + src2);
   INSTPAT("??????? ????? ????? 011 ????? 00100 11", sltiu  , I, R(rd) = src1 < ((uint32_t)(imm << 20) >> 20)); // notice imm of this should be zero-extended
-  //       0000000 00001 01010 011 01010 00100 11
+  INSTPAT("??????? ????? ????? 000 ????? 00100 11", beq    , B, s->dnpc += (src1 == src2 ? imm : 0));
+  //       0000000 00000 01010 000 01000 11000 11
+  //       TODO: bne
   // Testing extended instruction end
 
   INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
