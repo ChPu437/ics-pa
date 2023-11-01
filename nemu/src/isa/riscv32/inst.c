6b7b682691dac6c0887c0e37b4d91a8477da10a7
tracer-ics2022 <tracer@njuics.org>
2023-11-01 11:32:13 +0800

    >  compile NEMU 998244353 Jing Pu Linux jing-VirtualBox 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:32:13 up  1:27,  1 user,  load average: 0.03, 0.10, 0.06

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -139,6 +139,9 @@ static int decode_exec(Decode *s) {
 
   // // min3 --fail
 
+  // // load-store
+  INSTPAT("??????? ????? ????? 001 ????? 00000 11", lh     , I, R(rd) = Mr(src1 + imm, 2));
+
   // Testing extended instruction end
 
   INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
