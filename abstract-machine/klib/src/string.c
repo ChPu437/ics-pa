67ac52811fe51729087cdcd42be899054eecbde4
tracer-ics2022 <tracer@njuics.org>
2023-11-07 19:56:25 +0800

    >  compile NEMU 998244353 Jing Pu Linux jing-VirtualBox 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:56:25 up  1:31,  1 user,  load average: 0.28, 0.08, 0.02

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -27,22 +27,21 @@ char *strcat(char *dst, const char *src) {
   size_t len_dst = strlen(dst);
   size_t len_src = strlen(src);
   for (int i = 0; i < len_src; i++) {
-  	  *(dst + len_dst + i) = *(src + i);
+    *(dst + len_dst + i) = *(src + i);
   }
   *(dst + len_dst + len_src) = '\0';
   return dst;
 }
 
 int strcmp(const char *s1, const char *s2) {
-	size_t len_s1 = strlen(s1);
-	size_t len_s2 = strlen(s2);
-	size_t upper_bound = len_s1 < len_s2 ? len_s1 : len_s2;
-	for (int i = 0; i < upper_bound; i++) {
-		if (*(s1 + i) - *(s2 + i) != 0) {
-			return *(s1 + i) - *(s2 + i);
-		}	
-	}
-	return *(s1 + upper_bound) - *(s2 + upper_bound);
+  size_t len_s1 = strlen(s1);
+  size_t len_s2 = strlen(s2);
+  size_t upper_bound = len_s1 < len_s2 ? len_s1 : len_s2;
+  for (int i = 0; i < upper_bound; i++) {
+    if (*(s1 + i) == *(s2 + i)) continue;
+    return *(s1 + i) - *(s2 + i);
+  }
+  return *(s1 + upper_bound) - *(s2 + upper_bound);
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
@@ -50,10 +49,10 @@ int strncmp(const char *s1, const char *s2, size_t n) {
 }
 
 void *memset(void *s, int c, size_t n) {
-	for (int i = 0; i < n; i++) {
-		*((int*)s + i) = c;
-	}
-	return s;
+  for (int i = 0; i < n; i++) {
+    *((int*)s + i) = c;
+  }
+  return s;
 }
 
 void *memmove(void *dst, const void *src, size_t n) {
@@ -61,15 +60,19 @@ void *memmove(void *dst, const void *src, size_t n) {
 }
 
 void *memcpy(void *out, const void *in, size_t n) {
-	// char --> 1 byte
-	for (int i = 0; i < n; i++) {
-		*((char*)out + i) = *((char*)in + i); 
-	}
-	return out;
+  // char --> 1 byte
+  for (int i = 0; i < n; i++) {
+    *((char*)out + i) = *((char*)in + i); 
+  }
+  return out;
 }
 
 int memcmp(const void *s1, const void *s2, size_t n) {
-  panic("Not implemented");
+  for (int i = 0; i < n; i++) {
+    if(*((char*)s1 + i) == *((char*)s2 + i)) continue;
+	return *((char*)s1 + i) - *((char*)s2 + i);
+  }
+  return 0;
 }
 
 #endif
