98b48086b13afa23feb6bc6ea92bffda5255447c
tracer-ics2022 <tracer@njuics.org>
2023-11-07 20:29:18 +0800

    >  compile NEMU 998244353 Jing Pu Linux jing-VirtualBox 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:29:18 up  2:04,  1 user,  load average: 0.22, 0.08, 0.02

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -14,7 +14,29 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+	int cnt_write = 0;
+	va_list ap;
+	va_start(ap, fmt);
+
+	for (int i = 0; *(fmt + i) != '\0'; i++) {
+		if(*(fmt + i) == '%') {
+			switch(*(fmt + i + 1)) {
+				case '%':
+					*(out + (cnt_write++)) = '%';
+					break;
+				case 's':
+					break;
+				case 'd':
+					break;
+			}
+			i++; // jump over the format specifier, need to be expand to a certain value when the length of specifier gt 1
+		} else {
+			*(out + (cnt_write++)) = *(fmt + i);
+		}
+	}
+
+	va_end(ap);
+	return cnt_write;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
