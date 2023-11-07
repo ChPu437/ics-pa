dabb9dffe2ad3f9dc2590f738ba3f3f021bba23f
tracer-ics2022 <tracer@njuics.org>
2023-11-08 04:13:44 +0800

    >  compile NEMU 998244353 Jing Pu Linux jing-VirtualBox 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  04:13:44 up  6:09,  1 user,  load average: 0.02, 0.10, 0.07

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -6,6 +6,7 @@ void __am_timer_init() {
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
   uptime->us = inl(RTC_ADDR); inl(RTC_ADDR + 4);
+  // Not quite clear why must two reg be read at one time
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
