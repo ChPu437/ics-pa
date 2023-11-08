2151d30368fafb5842e48de4ad2a04025732454f
tracer-ics2022 <tracer@njuics.org>
2023-11-08 10:19:30 +0800

    >  compile NEMU 998244353 Jing Pu Linux jing-VirtualBox 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:19:30 up 25 min,  1 user,  load average: 0.23, 0.57, 0.41

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -70,7 +70,7 @@ void *memmove(void *dst, const void *src, size_t n) {
 		}
 	}
 	return dst;
-} // 估计有锅，ants跑不出来
+}
 
 void *memcpy(void *out, const void *in, size_t n) {
   panic("Not implemented");
