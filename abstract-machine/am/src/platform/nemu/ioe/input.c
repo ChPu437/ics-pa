434d5fca5da2ddb22bb8650f51a056bb2ae94ebf
tracer-ics2022 <tracer@njuics.org>
2023-11-08 14:42:54 +0800

    >  compile NEMU 998244353 Jing Pu Linux jing-VirtualBox 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:42:54 up 15 min,  1 user,  load average: 0.48, 0.45, 0.24

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -4,6 +4,7 @@
 #define KEYDOWN_MASK 0x8000
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  kbd->keycode = inl(KBD_ADDR) & ~KEYDOWN_MASK;
+  kbd->keycode = inl(KBD_ADDR);
   kbd->keydown = kbd->keycode & KEYDOWN_MASK;
+  kbd->keycode = kbd->keycode & ~KEYDOWN_MASK;
 }
