c800d19a0e8b5212a9b08f609a0afefa1740e0f0
tracer-ics2022 <tracer@njuics.org>
2023-11-15 11:18:58 +0800

    >  compile NEMU 998244353 Jing Pu Linux jing-VirtualBox 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:18:58 up  1:11,  1 user,  load average: 0.13, 0.05, 0.01

--- nemu/src/cpu/trace/mtrace.h
+++ nemu/src/cpu/trace/mtrace.h
@@ -1,12 +1,2 @@
-#include <stdio.h>
-#include <memory/paddr.h>
+// pls refer to $NEMU_HOME/src/memory/paddr.c
 
-// R/W addr_start(H) addr_end(H) length(xbyte) value(H)
-
-void mtrace_log(paddr_t addr, int len, word_t value, bool is_write) {
-	if (is_write)
-		printf("MEM: W\t");
-	else
-		printf("MEM: R\t");
-	printf("0x%08x\t0x%08x\t%-dB\t0x%08x\n", addr, addr + 8 * len, len, value);
-}
--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -19,7 +19,14 @@
 #include <isa.h>
 #include <stdio.h>
 #ifdef CONFIG_MTRACE
-#include "../cpu/trace/mtrace.h"
+// R/W addr_start(H) addr_end(H) length(xbyte) value(H)
+void mtrace_log(paddr_t addr, int len, word_t value, bool is_write) {
+	if (is_write)
+		printf("MEM: W\t");
+	else
+		printf("MEM: R\t");
+	printf("0x%08x\t0x%08x\t%-dB\t0x%08x\n", addr, addr + 8 * len, len, value);
+}
 #endif
 
 #if   defined(CONFIG_PMEM_MALLOC)
