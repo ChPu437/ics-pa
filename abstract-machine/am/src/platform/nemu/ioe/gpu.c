c74e75b8f9f8b961fd5770294be458da9a4be365
tracer-ics2022 <tracer@njuics.org>
2023-11-22 02:06:11 +0800

    >  compile NEMU 998244353 Jing Pu Linux jing-VirtualBox 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  02:06:11 up  1:20,  1 user,  load average: 1.09, 0.82, 0.54

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -16,7 +16,7 @@ void __am_gpu_init() {
   // 800 * 600 会爆栈
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
   // for (i = 0; i < w * 40; i ++) fb[i] = i / w * 20;
-	for (int i = 0; i < 100; i++) {
+	for (int i = 0; i < 400; i++) {
 		for (int j = 0; j < w; j++) {
 			fb[i * w + j] = i * 2;
 		}
