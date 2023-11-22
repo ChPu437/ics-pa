94d107a50f5c9103edecd90795d3bb18b2b73e64
tracer-ics2022 <tracer@njuics.org>
2023-11-22 14:57:16 +0800

    >  compile NEMU 998244353 Jing Pu Linux jing-VirtualBox 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:57:16 up  1:04,  1 user,  load average: 0.15, 0.51, 0.84

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -46,9 +46,9 @@ static void buf_flush() {
 	}
 	cnt_buf = 0;
 }
-/* static void buf_flush_str(char *out) { // out 应该是输出位置, 也即输出前的'\0'
+// static void buf_flush_str() {
 
-} */
+// }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
 	int cnt_write = 0;
@@ -234,10 +234,10 @@ int printf(const char *fmt, ...) {
 
 			// flush buffer and output to stdout
 			cnt_write += cnt_buf;
-			if (io_format.flag & FLAG_LEFT_ALIGN) {
+			if (io_format.flag != FLAG_LEFT_ALIGN) {
 				if(cnt_buf < io_format.width) {
 					for (int i = io_format.width - cnt_buf; i > 0; i--) {
-						putch(io_format.flag & FLAG_ZERO_PADDING ? '0' : ' ');
+						putch(io_format.flag==FLAG_ZERO_PADDING ? '0' : ' ');
 					}
 				}
 				buf_flush();
