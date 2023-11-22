fde53ab67587a8c1328ab4fa43ba22a2e337bd52
tracer-ics2022 <tracer@njuics.org>
2023-11-22 15:25:30 +0800

    >  compile NEMU 998244353 Jing Pu Linux jing-VirtualBox 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:25:30 up  1:33,  1 user,  load average: 0.16, 0.12, 0.22

--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -75,11 +75,11 @@ void vga_update_screen() {
   // TODO: call `update_screen()` when the sync register is non-zero,
   // then zero out the sync register
   // if (mmio_read(CONFIG_VGA_CTL_MMIO + 4, 1)) { // am/ioe/gpu.c: SYNC_ADDR = (VGACTL_ADDR + 4)
-  if (vgactl_port_base[5]) { // am/ioe/gpu.c: SYNC_ADDR = (VGACTL_ADDR + 4)
+  if (vgactl_port_base[3]) { // am/ioe/gpu.c: SYNC_ADDR = (VGACTL_ADDR + 4)
 //		printf("!vga sync\n");
 	  update_screen();
 	  // mmio_write(CONFIG_VGA_CTL_MMIO + 4, 1, 0);
-	  vgactl_port_base[5] = 0;
+	  vgactl_port_base[3] = 0;
   }
 }
 
