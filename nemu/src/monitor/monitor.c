33bd2eb16937b9dfb59bd31464669cf4094bc387
tracer-ics2022 <tracer@njuics.org>
2023-11-29 23:20:29 +0800

    >  compile NEMU 998244353 Jing Pu Linux jing-VirtualBox 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:20:29 up  1:23,  1 user,  load average: 0.10, 0.23, 0.20

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -51,9 +51,7 @@ static char *diff_so_file = NULL;
 static char *img_file = NULL;
 static int difftest_port = 1234;
 
-#ifdef CONFIG_FTRACE
 static char* ftrace_file = NULL;
-#endif
 
 static long load_img() {
   if (img_file == NULL) {
@@ -94,17 +92,13 @@ static int parse_args(int argc, char *argv[]) {
       case 'p': sscanf(optarg, "%d", &difftest_port); break;
       case 'l': log_file = optarg; break;
       case 'd': diff_so_file = optarg; break;
-#ifdef CONFIG_FTRACE
-			case 'f': ftrace_file = optarg; assert(0); break;
-#endif
+			case 'f': ftrace_file = optarg; break;
       case 1: img_file = optarg; return 0;
       default:
         printf("Usage: %s [OPTION...] IMAGE [args]\n\n", argv[0]);
         printf("\t-b,--batch              run with batch mode\n");
         printf("\t-l,--log=FILE           output log to FILE\n");
-#ifdef CONFIG_FTRACE
         printf("\t-f,--ftrace=FILE      run with ftrace enabled, use current file for elf header\n");
-#endif
         printf("\t-d,--diff=REF_SO        run DiffTest with reference REF_SO\n");
         printf("\t-p,--port=PORT          run DiffTest with port PORT\n");
         printf("\n");
