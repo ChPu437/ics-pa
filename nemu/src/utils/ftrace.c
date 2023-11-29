091d180ca9bdf8873e47a4f01b9efa4e28bd98de
tracer-ics2022 <tracer@njuics.org>
2023-11-29 22:17:49 +0800

    >  compile NEMU 998244353 Jing Pu Linux jing-VirtualBox 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:17:49 up 20 min,  1 user,  load average: 0.34, 0.15, 0.06

--- nemu/src/utils/ftrace.c
+++ nemu/src/utils/ftrace.c
@@ -14,7 +14,7 @@
 
 bool g_f_init = 0;
 static Elf32_Ehdr g_elf_header;
-static Elf32_Shdr g_section_header[FTRACE_MAX_SH_SIZE];
+// static Elf32_Shdr g_section_header[FTRACE_MAX_SH_SIZE];
 // static char g_f_strtab[FTRACE_STRTAB_SIZE][FTRACE_MAX_STR_LENG];
 
 void init_ftrace(const char *elf_file) {
@@ -30,8 +30,8 @@ void init_ftrace(const char *elf_file) {
 
 	// TODO: this didn't work (deadloop)
 	fseek(fp, g_elf_header.e_shoff, SEEK_SET);
-	success = fread(g_section_header, sizeof(Elf32_Shdr), g_elf_header.e_shnum, fp);
-	// assert(success);
+	// success = fread(g_section_header, sizeof(Elf32_Shdr), g_elf_header.e_shnum, fp);
+	assert(success);
 	// TODO: parse string tab 
 
 	// TODO: parse symbol tab
