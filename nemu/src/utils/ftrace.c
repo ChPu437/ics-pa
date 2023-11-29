ee1fd93642230b91d0c72aaf931dc55992fdf5a9
tracer-ics2022 <tracer@njuics.org>
2023-11-29 11:43:44 +0800

    >  compile NEMU 998244353 Jing Pu Linux jing-VirtualBox 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:43:44 up  1:52,  1 user,  load average: 0.53, 0.25, 0.11

--- nemu/src/utils/ftrace.c
+++ nemu/src/utils/ftrace.c
@@ -39,7 +39,7 @@ void init_ftrace(const char *elf_file) {
 	fseek(fp, g_elf_header.e_shoff, SEEK_SET);
 	do { // located to symtab entry
 		success = fread(&g_section_header, sizeof(Elf32_Shdr), 1, fp);
-		assert(success);
+		// assert(success);
 	} while (g_section_header.sh_type != SHT_SYMTAB);
 	// TODO: parse symbol tab
 	// // map name from strtab
