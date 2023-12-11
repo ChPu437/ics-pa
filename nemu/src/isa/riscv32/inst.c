92478fa2cad82e30fb74a04be6fa5c3db35f9941
tracer-ics2022 <tracer@njuics.org>
2023-12-12 04:25:26 +0800

    >  compile NEMU 998244353 Jing Pu Linux jing-VirtualBox 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  04:25:26 up  1:29,  1 user,  load average: 0.37, 0.32, 0.19

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -175,7 +175,7 @@ static int decode_exec(Decode *s) {
   // INSTPAT("??????? ????? ????? 001 00000 11100 11", csrw   , CSR, R(rd) = ); // csrrw, rd == x0 即为 csrw
   // x0 本来就写不进去，没必要单独列一条指令
   INSTPAT("??????? ????? ????? 001 ????? 11100 11", csrrw   , CSR, R(rd) = ZEXT(CSRR(imm), 32), CSRW(imm) = src1); // csrrw, rd == x0 即为 csrw
-  // INSTPAT("0000000 00000 00000 000 00000 11100 11", ecall  , I, isa_raise_intr(R(17), s->pc)); // R(17) == a7
+  INSTPAT("0000000 00000 00000 000 00000 11100 11", ecall  , I, isa_raise_intr(R(17), s->pc)); // R(17) == a7
 
   // Extended instruction end
 
@@ -189,7 +189,7 @@ static int decode_exec(Decode *s) {
 
   // Testing extended instruction end
 
-  // INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
+  INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
   																						 
   // Notice that you SHOULDN'T place ANY instruction under the instruction below, since it matches ANY sequence
   INSTPAT("??????? ????? ????? ??? ????? ????? ??", inv    , N, INV(s->pc));
