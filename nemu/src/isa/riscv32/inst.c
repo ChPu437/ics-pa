ec445401b535c03d0f922c889ff04513f32da84f
tracer-ics2022 <tracer@njuics.org>
2023-12-12 04:42:15 +0800

    >  compile NEMU 998244353 Jing Pu Linux jing-VirtualBox 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  04:42:15 up  1:46,  1 user,  load average: 0.06, 0.11, 0.13

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -174,8 +174,8 @@ static int decode_exec(Decode *s) {
 #define CSRW(i) *csr_write(i)
   // INSTPAT("??????? ????? ????? 001 00000 11100 11", csrw   , CSR, R(rd) = ); // csrrw, rd == x0 即为 csrw
   // x0 本来就写不进去，没必要单独列一条指令
-  INSTPAT("??????? ????? ????? 001 ????? 11100 11", csrrw   , CSR, R(rd) = ZEXT(CSRR(imm), 32), CSRW(imm) = src1,assert(CSRR(imm) == 0x800013A8)); // csrrw, rd == x0 即为 csrw
-  INSTPAT("0000000 00000 00000 000 00000 11100 11", ecall  , I, isa_raise_intr(R(17), s->pc)); // R(17) == a7
+  INSTPAT("??????? ????? ????? 001 ????? 11100 11", csrrw   , CSR, R(rd) = ZEXT(CSRR(imm), 32), CSRW(imm) = src1); // csrrw, rd == x0 即为 csrw
+  INSTPAT("0000000 00000 00000 000 00000 11100 11", ecall  , I, s->dnpc = isa_raise_intr(R(17), s->pc)); // R(17) == a7
 
   // Extended instruction end
 
