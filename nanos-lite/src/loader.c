bc7a9bcfd8be1b163906bf08630adf9edfc56a01
tracer-ics2022 <tracer@njuics.org>
2023-12-13 03:01:36 +0800

    >  compile NEMU 998244353 Jing Pu Linux jing-VirtualBox 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  03:01:36 up  5:53,  1 user,  load average: 0.10, 0.11, 0.04

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -53,6 +53,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
+	assert(0);
   uintptr_t entry = loader(pcb, filename);
   Log("Jump to entry = %p", entry);
   ((void(*)())entry) ();
