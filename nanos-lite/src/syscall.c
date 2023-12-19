78760a8f7b4c0e1f0cfc00ce5b46ff9b2136a466
tracer-ics2022 <tracer@njuics.org>
2023-12-20 02:56:38 +0800

    >  compile NEMU 998244353 Jing Pu Linux jing-VirtualBox 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  02:56:38 up  1:47,  1 user,  load average: 0.26, 0.24, 0.09

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,16 +1,18 @@
 #include <common.h>
 #include "syscall.h"
 
-#define SYS_exit 0
-#define SYS_yield 1
+#define SYS_EXIT 0
+#define SYS_YIELD 1
+#define SYS_WRITE 2
 
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
 
   switch (a[0]) {
-		case SYS_exit:  sys_exit(c);  break;
-		case SYS_yield: sys_yield(c); break;
+		case SYS_EXIT:  sys_exit(c);  break;
+		case SYS_YIELD: sys_yield(c); break;
+		case SYS_WRITE: sys_write(c); break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 }
