03ae17c00ae52b3b8f4158e5c8299c1ac515cc99
tracer-ics2022 <tracer@njuics.org>
2023-12-20 03:40:30 +0800

    >  compile NEMU 998244353 Jing Pu Linux jing-VirtualBox 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  03:40:30 up  2:30,  1 user,  load average: 0.16, 0.13, 0.12

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -63,6 +63,7 @@ int _open(const char *path, int flags, mode_t mode) {
 
 int _write(int fd, void *buf, size_t count) {
   return _syscall_(SYS_write, fd, (intptr_t)buf, count);
+  // 这里注意navy-apps的c库的实现是一个个write，看上一个write是否成功再write下一个
 }
 
 void *_sbrk(intptr_t increment) {
