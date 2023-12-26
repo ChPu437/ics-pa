be7d1f638903369e06894a9961393a0bd64a1adf
tracer-ics2022 <tracer@njuics.org>
2023-12-27 02:57:26 +0800

    >  compile NEMU 998244353 Jing Pu Linux jing-VirtualBox 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  02:57:26 up  3:54,  1 user,  load average: 0.28, 0.08, 0.02

--- nanos-lite/src/custom_syscall.h
+++ nanos-lite/src/custom_syscall.h
@@ -7,6 +7,14 @@ void sys_exit() {
 	halt(0);	
 }
 
+int32_t sys_open(const char *pathname, int flags, int mode) {
+#ifdef HAS_NAVY
+	return fs_open(pathname, flags, mode);
+#else
+	return -1;
+#endif
+}
+
 int32_t sys_read(int fd, char* buf, size_t count) {
 #ifdef HAS_NAVY
 	return fs_read(fd, buf, count);
@@ -26,6 +34,22 @@ int32_t sys_write(int fd, const char* buf, size_t count) {
 		putch(*(buf + i));
 	}
 	return count;
+#endif
+}
+
+int32_t sys_close(int fd) {
+#ifdef HAS_NAVY
+	return fs_close(fd);
+#else
+	return -1;
+#endif
+}
+
+int32_t sys_lseek(int fd, size_t offset, int whence) {
+#ifdef HAS_NAVY
+	return fs_lseek(fd, offset, whence);
+#else 
+	return -1;
 #endif
 }
 
--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -14,13 +14,13 @@ void do_syscall(Context *c) {
   a[0] = c->GPR1;
 
   switch (a[0]) {
-		case SYS_exit:            sys_exit();  break;
+		case SYS_exit:            sys_exit(); break;
 		case SYS_yield: c->GPRx = sys_yield(); break;
-		case SYS_open:
-		case SYS_read:  c->GPRx = sys_read(c->GPR2, (char*)c->GPR3, (size_t)c->GPR4); break;
+		case SYS_open:  c->GPRx = sys_open((const char*)c->GPR2, (int)c->GPR3, (int)c->GPR4); break;
+		case SYS_read:  c->GPRx = sys_read((int)c->GPR2, (char*)c->GPR3, (size_t)c->GPR4); break;
 		case SYS_write: c->GPRx = sys_write((int)c->GPR2, (const char*)c->GPR3, (size_t)c->GPR4); break;
-		case SYS_close:
-		case SYS_lseek:
+		case SYS_close: c->GPRx = sys_close((int)c->GPR2); break;
+		case SYS_lseek: c->GPRx = sys_lseek((int)c->GPR2, (size_t)c->GPR3, (int)c->GPR4); break;
 		case SYS_brk:   c->GPRx = sys_brk((intptr_t)c->GPR2); break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
