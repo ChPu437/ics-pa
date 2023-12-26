a7add2b546aa9ceed0200276093ac8d7c438c781
tracer-ics2022 <tracer@njuics.org>
2023-12-27 03:13:58 +0800

    >  compile NEMU 998244353 Jing Pu Linux jing-VirtualBox 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  03:13:58 up  4:11,  1 user,  load average: 0.08, 0.07, 0.03

--- nanos-lite/src/custom_syscall.h
+++ nanos-lite/src/custom_syscall.h
@@ -1,3 +1,9 @@
+extern size_t fs_open(const char*, int, int);
+extern size_t fs_read(int, char*, size_t);
+extern size_t fs_write(int, const char*, size_t);
+extern int fs_close(int);
+extern size_t fs_lseek(int, size_t, int);
+
 int32_t sys_yield() {
 	yield();
 	return 0;
@@ -8,50 +14,30 @@ void sys_exit() {
 }
 
 int32_t sys_open(const char *pathname, int flags, int mode) {
-#ifdef HAS_NAVY
 	return fs_open(pathname, flags, mode);
-#else
-	assert(0);
-	return -1;
-#endif
 }
 
 int32_t sys_read(int fd, char* buf, size_t count) {
-#ifdef HAS_NAVY
 	return fs_read(fd, buf, count);
-#else
-	return -1;
-#endif
 }
 
 int32_t sys_write(int fd, const char* buf, size_t count) {
-#ifdef HAS_NAVY
 	return fs_write(fd, buf, count);
-#else
-	if (fd != 1 && fd != 2) {
+/*	if (fd != 1 && fd != 2) {
 		assert(0);
 	}
 	for (int i = 0; i < count; i++) {
 		putch(*(buf + i));
 	}
-	return count;
-#endif
+	return count; */
 }
 
 int32_t sys_close(int fd) {
-#ifdef HAS_NAVY
 	return fs_close(fd);
-#else
-	return -1;
-#endif
 }
 
 int32_t sys_lseek(int fd, size_t offset, int whence) {
-#ifdef HAS_NAVY
 	return fs_lseek(fd, offset, whence);
-#else 
-	return -1;
-#endif
 }
 
 int32_t sys_brk(intptr_t addr) {
