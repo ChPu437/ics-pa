d603a8ab86e37eb016fbcf3c73ce67c42584f6d5
tracer-ics2022 <tracer@njuics.org>
2023-12-27 03:35:27 +0800

    >  compile NEMU 998244353 Jing Pu Linux jing-VirtualBox 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  03:35:27 up  4:32,  1 user,  load average: 0.25, 0.17, 0.11

--- nanos-lite/src/custom_fs.h
+++ nanos-lite/src/custom_fs.h
@@ -14,7 +14,7 @@ int fs_open(const char *pathname, int flags, int mode) {
 size_t fs_read(int fd, void *buf, size_t len) {
 	assert(fd >= 0 && fd <= file_count);
 
-	if (len + file_table[fd].open_offset > file_table[fd].size)
+	if (len + file_table[fd].open_offset > file_table[fd].size + 1)
 		return -1;
 	ramdisk_read(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
 	file_table[fd].open_offset += len;
