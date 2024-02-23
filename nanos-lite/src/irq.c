024f121505bb560ef78cc1dc507907817389e66b
tracer-ics2022 <tracer@njuics.org>
2024-02-23 15:20:44 +0800

    >  compile NEMU 998244353 Jing Pu Linux jing-VirtualBox 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:20:44 up 14 min,  1 user,  load average: 0.63, 0.53, 0.31

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -4,8 +4,8 @@ extern void do_syscall(Context *c);
 
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
-		case EVENT_YIELD: Log("yield event detected!"); break;
-		case EVENT_SYSCALL: Log("syscall event detected!"); do_syscall(c); break;
+		case EVENT_YIELD: /*Log("yield event detected!");*/ break;
+		case EVENT_SYSCALL: /*Log("syscall event detected!");*/ do_syscall(c); break;
 		case EVENT_ERROR: panic("Error event");
     default: panic("Unhandled event ID = %d", e.event);
   }
