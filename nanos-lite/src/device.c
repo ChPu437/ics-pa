5cc32fb8e8b1155570294638ca5475b32e94cea4
tracer-ics2022 <tracer@njuics.org>
2024-02-26 17:08:43 +0800

    >  compile NEMU 998244353 Jing Pu Linux jing-VirtualBox 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:08:43 up  1:13,  1 user,  load average: 0.42, 0.12, 0.07

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -15,14 +15,36 @@ static const char *keyname[256] __attribute__((used)) = {
 };
 
 size_t serial_write(const void *buf, size_t offset, size_t len) {
+	assert(offset == 0); // 对于stdout和stderr，offset应该始终为0
   for (int i = 0; i < len; i++) {
-		putch(*((char*)buf + i));
+		putch(*((char*)buf + offset + i));
 	}
 	return len;
 }
 
 size_t events_read(void *buf, size_t offset, size_t len) {
-  return 0;
+	// 读出一条事件信息
+	AM_INPUT_KEYBRD_T ev = io_read(AM_INPUT_KEYBRD);
+	if (ev.keycode == AM_KEY_NONE) return 0; // 没有按下、松开按键，无有效事件
+
+	if (len) ((char*)buf)[offset + 0] = 'k';
+	len--;
+	if (ev.keydown) {
+		if (len) ((char*)buf)[offset + 1] = 'd';
+		len--;
+	} else {
+		if (len) ((char*)buf)[offset + 1] = 'u';
+		len--;
+	}
+	if (len) ((char*)buf)[offset + 2] = ' ';
+	len--;
+
+	offset += 3;
+	for (int i = 0; len > 0; i++, len--) {
+		((char*)buf)[offset + i] = keyname[ev.keycode][i];
+	}
+
+  return 1;
 }
 
 size_t dispinfo_read(void *buf, size_t offset, size_t len) {
