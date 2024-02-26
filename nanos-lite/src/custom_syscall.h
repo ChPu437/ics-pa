fe91eb712a66ffe04a2a22bfc4eccffac92b5ee0
tracer-ics2022 <tracer@njuics.org>
2024-02-26 14:55:30 +0800

    >  compile NEMU 998244353 Jing Pu Linux jing-VirtualBox 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:55:30 up 39 min,  1 user,  load average: 1.13, 0.50, 0.27

--- nanos-lite/src/custom_syscall.h
+++ nanos-lite/src/custom_syscall.h
@@ -61,9 +61,9 @@ int32_t sys_gettimeofday(void* tp, void* tzp) {
 	struct timeval* _tp = tp;
 
 	int cycles = rtc.year / 4;
+	_tp->tv_sec = cycles * (_secs_even_year + 3 * _secs_odd_year) + (rtc.year % 4) * _secs_odd_year;
 
 	Log("gettimeofday finished!");
-	_tp->tv_sec = cycles * (_secs_even_year + 3 * _secs_odd_year) + (rtc.year % 4) * _secs_odd_year;
 	_tp->tv_usec = _tp->tv_sec * 1e6;
 	
 	return 0;
