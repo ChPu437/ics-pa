ef26471567b2eaa01d67abbcabd48a67ae3b9b19
tracer-ics2022 <tracer@njuics.org>
2024-02-28 09:34:02 +0800

    >  compile NEMU 998244353 Jing Pu Linux jing-VirtualBox 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:34:02 up  9:42,  1 user,  load average: 0.20, 0.14, 0.10

--- navy-apps/apps/nslider/src/main.cpp
+++ navy-apps/apps/nslider/src/main.cpp
@@ -20,15 +20,15 @@ static SDL_Surface *slide = NULL;
 static int cur = 0;
 
 void render() {
-	printf("into render!");
+	printf("into render!\n");
   if (slide) {
     SDL_FreeSurface(slide);
   }
   char fname[256];
   sprintf(fname, path, cur);
-	printf("into loadbmp!");
+	printf("into loadbmp!\n");
   slide = SDL_LoadBMP(fname);
-  printf("into assert!");
+  printf("into assert!\n");
   assert(slide);
   SDL_UpdateRect(slide, 0, 0, 0, 0);
 }
