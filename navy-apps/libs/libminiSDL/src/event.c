66336fc8756b35444d73f60329f7eddb6be450aa
tracer-ics2022 <tracer@njuics.org>
2024-02-28 13:48:46 +0800

    >  compile NEMU 998244353 Jing Pu Linux jing-VirtualBox 6.5.0-21-generic #21~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb  9 13:32:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:48:46 up 36 min,  1 user,  load average: 0.28, 0.13, 0.05

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -19,12 +19,28 @@ int SDL_PollEvent(SDL_Event *ev) {
 }
 
 int SDL_WaitEvent(SDL_Event *event) {
-	char buf[32], key[32];
+	char buf[32];
+	int* sym = &event->key.keysym.sym;
 	while (!NDL_PollEvent(buf, sizeof(buf) + 1));
 	switch (buf[1]) {
 		case 'd':
 			event->type = SDL_KEYDOWN;
-			event->key.keysym.sym;
+			// TODO: regex-ify the compare
+			if (strcmp(buf, "kd UP")) 	*sym = SDLK_UP;
+			if (strcmp(buf, "kd DOWN")) *sym = SDLK_DOWN;
+			if (strcmp(buf, "kd 0")) 		*sym = SDLK_0;
+			if (strcmp(buf, "kd 1")) 		*sym = SDLK_1;
+			if (strcmp(buf, "kd 2")) 		*sym = SDLK_2;
+			if (strcmp(buf, "kd 3")) 		*sym = SDLK_3;
+			if (strcmp(buf, "kd 4")) 		*sym = SDLK_4;
+			if (strcmp(buf, "kd 5")) 		*sym = SDLK_5;
+			if (strcmp(buf, "kd 6")) 		*sym = SDLK_6;
+			if (strcmp(buf, "kd 7")) 		*sym = SDLK_7;
+			if (strcmp(buf, "kd 8")) 		*sym = SDLK_8;
+			if (strcmp(buf, "kd 9")) 		*sym = SDLK_9;
+			if (strcmp(buf, "kd G")) 		*sym = SDLK_G;
+			if (strcmp(buf, "kd J")) 		*sym = SDLK_J;
+			if (strcmp(buf, "kd K")) 		*sym = SDLK_K;
 			break;
 		case 'u':
 			event->type = SDL_KEYUP;
