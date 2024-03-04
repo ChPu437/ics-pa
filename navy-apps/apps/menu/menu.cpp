570624cc45fc431d6e4d801ffd069228fd6a92d0
tracer-ics2022 <tracer@njuics.org>
2024-03-04 16:47:03 +0800

    >  compile NEMU 998244353 Jing Pu Linux jing-VirtualBox 6.5.0-21-generic #21~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb  9 13:32:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:47:02 up 11 min,  1 user,  load average: 1.25, 0.67, 0.31

--- navy-apps/apps/menu/menu.cpp
+++ navy-apps/apps/menu/menu.cpp
@@ -68,7 +68,7 @@ static void clear_display(void) {
 }
 
 int main(int argc, char *argv[], char *envp[]) {
-	printf("%s\n", font_name);
+	printf("%s\n", font_fname);
   SDL_Init(0);
   screen = SDL_SetVideoMode(0, 0, 32, SDL_HWSURFACE);
 
