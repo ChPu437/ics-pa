368eccb49e08de198f60e9a9951f64d0f12273aa
tracer-ics2022 <tracer@njuics.org>
2024-03-06 01:18:52 +0800

    >  compile NEMU 998244353 Jing Pu Linux jing-VirtualBox 6.5.0-21-generic #21~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb  9 13:32:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  01:18:52 up  1:11,  1 user,  load average: 1.18, 0.59, 0.42

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -36,10 +36,13 @@ void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_
 	SDL_LockSurface(src);
 	SDL_LockSurface(dst); 
 	for (int i = 0; i < copy_h; i++) {
-		for (int j = 0; j < copy_w; j++) {
-			dst->pixels[(i + dst_y) * (dst->w) + (j + dst_x)] = src->pixels[(i + copy_y) * (src->w) + (j + copy_x)];
+		// for (int j = 0; j < copy_w; j++) {
+			// dst->pixels[(i + dst_y) * (dst->w) + (j + dst_x)] = src->pixels[(i + copy_y) * (src->w) + (j + copy_x)];
 			// printf("%d %d %d %d\n", j + dst_x, i + dst_y, j + copy_x, i + copy_y);
-		}
+		// }
+		memcpy(dst->pixels + (i + dst_y) * dst->w + dst_x,
+				src->pixels + (i + copy_y) * src->w + copy_x,
+				sizeof(dst->pixels[0]));
 	}
 	SDL_UnlockSurface(src);
 	SDL_UnlockSurface(dst); 
