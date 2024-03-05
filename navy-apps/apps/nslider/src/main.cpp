ae7e266066551096eeb36fca2b6b0f679c8c1219
tracer-ics2022 <tracer@njuics.org>
2024-03-05 12:22:05 +0800

    >  compile NEMU 998244353 Jing Pu Linux jing-VirtualBox 6.5.0-21-generic #21~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb  9 13:32:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:22:04 up  1:49,  1 user,  load average: 1.26, 0.86, 0.60

--- navy-apps/apps/nslider/src/main.cpp
+++ navy-apps/apps/nslider/src/main.cpp
@@ -48,6 +48,8 @@ void next(int rep) {
 
 int main() {
 	printf("%d %p\n", cur, slide);
+	cur = 0;
+	slide = NULL;
 
   SDL_Init(0);
   SDL_Surface *screen = SDL_SetVideoMode(W, H, 32, SDL_HWSURFACE);
