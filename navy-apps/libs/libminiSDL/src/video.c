8e3d9571ec09a0dedb281506c7abb8a5372ad241
tracer-ics2022 <tracer@njuics.org>
2024-03-06 02:31:05 +0800

    >  compile NEMU 998244353 Jing Pu Linux jing-VirtualBox 6.5.0-21-generic #21~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb  9 13:32:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  02:31:05 up  2:23,  1 user,  load average: 0.50, 0.52, 0.66

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -101,8 +101,9 @@ void SDL_UpdateRect(SDL_Surface *s, int x, int y, int w, int h) {
 		printf("8-bit color detected!\n");
 		for (int i = 0; i < w * h; i++) {
 			int pos = (y * w + x + i) * 4;
-			printf("%x %x %x %x\n", s->pixels[pos], s->pixels[pos+1], s->pixels[pos+2], s->pixels[pos+3]);
+			// printf("%x %x %x %x\n", s->pixels[pos], s->pixels[pos+1], s->pixels[pos+2], s->pixels[pos+3]);
 			buf[i] = (s->pixels[pos + 0] << 16) | (s->pixels[pos + 1] << 8) | (s->pixels[pos + 2]);
+			printf("%X\n", buf[i]);
 		}
 	} else {
 		NDL_TODO("format not implemented!");
