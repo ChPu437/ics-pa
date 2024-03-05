5fa9b6a9ef3f8472ca0ec3d1411a9eb9fa93b4c1
tracer-ics2022 <tracer@njuics.org>
2024-03-05 11:28:49 +0800

    >  compile NEMU 998244353 Jing Pu Linux jing-VirtualBox 6.5.0-21-generic #21~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb  9 13:32:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:28:49 up 56 min,  1 user,  load average: 0.61, 0.37, 0.46

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -9,6 +9,41 @@ static const char *keyname[] = {
   _KEYS(keyname)
 };
 
+static int key_assert(char* buf, SDL_Event *event) {
+		uint8_t* sym = &event->key.keysym.sym;
+	// printf("SDLK: %d\n", event->key.keysym.sym);
+	switch (buf[1]) {
+		case 'd':
+			event->type = SDL_KEYDOWN;
+			break;
+		case 'u':
+			event->type = SDL_KEYUP;
+			break;
+		default:
+			printf("Event not implemented\n");
+			NDL_TODO("");
+			break;
+	}
+	// TODO: regex-ify the compare
+	if (!strcmp(buf, "kd UP")) 		*sym = SDLK_UP;
+	if (!strcmp(buf, "kd DOWN")) 	*sym = SDLK_DOWN;
+	if (!strcmp(buf, "kd LEFT")) 	*sym = SDLK_LEFT;
+	if (!strcmp(buf, "kd RIGHT")) *sym = SDLK_RIGHT;
+	if (!strcmp(buf, "kd 2")) 		*sym = SDLK_2;
+	if (!strcmp(buf, "kd 3")) 		*sym = SDLK_3;
+	if (!strcmp(buf, "kd 4")) 		*sym = SDLK_4;
+	if (!strcmp(buf, "kd 5")) 		*sym = SDLK_5;
+	if (!strcmp(buf, "kd 6")) 		*sym = SDLK_6;
+	if (!strcmp(buf, "kd 7")) 		*sym = SDLK_7;
+	if (!strcmp(buf, "kd 8")) 		*sym = SDLK_8;
+	if (!strcmp(buf, "kd 9")) 		*sym = SDLK_9;
+	if (!strcmp(buf, "kd G")) 		*sym = SDLK_G;
+	if (!strcmp(buf, "kd J")) 		*sym = SDLK_J;
+	if (!strcmp(buf, "kd K")) 		*sym = SDLK_K;
+
+	return 0;
+}
+
 int SDL_PushEvent(SDL_Event *ev) {
 	NDL_TODO("");
   return 0;
@@ -22,35 +57,7 @@ int SDL_PollEvent(SDL_Event *ev) {
 int SDL_WaitEvent(SDL_Event *event) {
 	char buf[32];
 	while (!NDL_PollEvent(buf, sizeof(buf) + 1));
-	uint8_t* sym = &event->key.keysym.sym;
-	// printf("SDLK: %d\n", event->key.keysym.sym);
-	switch (buf[1]) {
-		case 'd':
-			event->type = SDL_KEYDOWN;
-			// TODO: regex-ify the compare
-			// if (strcmp(buf, "kd UP")) 	*sym = SDLK_UP;
-			if (!strcmp(buf, "kd UP")) event->key.keysym.sym = SDLK_UP;
-			if (!strcmp(buf, "kd DOWN")) event->key.keysym.sym = SDLK_DOWN;
-			if (!strcmp(buf, "kd 2")) 		*sym = SDLK_2;
-			if (!strcmp(buf, "kd 3")) 		*sym = SDLK_3;
-			if (!strcmp(buf, "kd 4")) 		*sym = SDLK_4;
-			if (!strcmp(buf, "kd 5")) 		*sym = SDLK_5;
-			if (!strcmp(buf, "kd 6")) 		*sym = SDLK_6;
-			if (!strcmp(buf, "kd 7")) 		*sym = SDLK_7;
-			if (!strcmp(buf, "kd 8")) 		*sym = SDLK_8;
-			if (!strcmp(buf, "kd 9")) 		*sym = SDLK_9;
-			if (!strcmp(buf, "kd G")) 		*sym = SDLK_G;
-			if (!strcmp(buf, "kd J")) 		*sym = SDLK_J;
-			if (!strcmp(buf, "kd K")) 		*sym = SDLK_K;
-			break;
-		case 'u':
-			event->type = SDL_KEYUP;
-			break;
-		default:
-			printf("Event not implemented\n");
-			NDL_TODO("");
-			break;
-	}
+	key_assert(buf, event);
 	// printf("SDLK: %d\n", event->key.keysym.sym);
   return 1;
 }
