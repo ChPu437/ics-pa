5403a344ce1a0828fb99756deb204f579220c45f
tracer-ics2022 <tracer@njuics.org>
2024-03-12 16:54:28 +0800

    >  compile NEMU 998244353 Jing Pu Linux jing-VirtualBox 6.5.0-25-generic #25~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Feb 20 16:09:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:54:28 up 20 min,  1 user,  load average: 0.24, 0.17, 0.16

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -47,19 +47,19 @@ void init_proc() {
 
 Context* schedule(Context *prev) {
   current->cp = prev;
-	if (current == &pcb[MAX_NR_PROC - 1]) {
-		printf("1\n");
-		current = &pcb[0];
-	} else if (current + 1 == NULL) {
-		printf("2\n");
-		current = &pcb[0];
-	} else if (current == &pcb_boot) {
-		printf("3\n");
-		current = &pcb[0];
+	if (current == &pcb_boot) {
+
 	} else {
-		printf("4\n");
-		current = current + 1;
+		PCB* i = NULL;
+		for (i = current; i < &pcb[4]; i++) {
+			if (i != NULL) break;
+		}
+		for (i = &pcb[0]; i < current; i++) {
+			if (i != NULL) break;
+		}
+		assert(i != NULL);
+		current = i;
 	}
-  return current->cp;
+	return current->cp;
 }
 
