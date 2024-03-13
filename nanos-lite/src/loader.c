241c0d3896a611fce2f907cc9c0e71ac3e80817c
tracer-ics2022 <tracer@njuics.org>
2024-03-13 13:46:56 +0800

    >  compile NEMU 998244353 Jing Pu Linux jing-VirtualBox 6.5.0-25-generic #25~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Feb 20 16:09:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:46:56 up  3:12,  1 user,  load average: 0.37, 0.30, 0.18

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -84,7 +84,7 @@ void context_uload(PCB *_pcb, const char *filename) {
 	uintptr_t entry = loader(NULL, filename);
 	_pcb->cp = ucontext(NULL, (Area){(void*)_pcb, (void*)((uintptr_t)_pcb + sizeof(PCB))}, (void*)entry);
 	// _pcb->cp->GPRx = (uintptr_t)heap.end;
-	// _pcb->cp->gpr[2] = (uintptr_t)heap.end;
+	_pcb->cp->gpr[2] = (uintptr_t)heap.end;
 	Log("Program = \"%s\" registered with Entry = %p\n", filename, entry);
 	// ((void(*)())entry) ();
 	return;
