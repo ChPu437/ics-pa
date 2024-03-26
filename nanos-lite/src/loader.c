51a78816f596c55e2b1ecdf5e73d5c6336637f37
tracer-ics2022 <tracer@njuics.org>
2024-03-26 11:37:23 +0800

    >  compile NEMU 998244353 Jing Pu Linux jing-VirtualBox 6.5.0-25-generic #25~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Feb 20 16:09:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:37:23 up 35 min,  1 user,  load average: 0.71, 0.74, 0.72

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -86,9 +86,8 @@ void context_uload(PCB *_pcb, const char *filename, char* const argv[], char* co
 	_pcb->cp = ucontext(NULL, (Area){(void*)_pcb, (void*)((uintptr_t)_pcb + sizeof(PCB))}, (void*)entry);
 	// ？：这里用户栈顶应该是可用空间的顶，为了防止数据覆写，实际上预分配的空间应该留空，所以栈顶位置应该恰好是argc的位置？
 
-	void* ustack_end = heap.end;
+	void* ustack_end = heap.end - 1;
 	Log("ustack_end = %X", ustack_end);
-	ustack_end--;
 
 	int argc = 0;
 	for (; argv[argc] != NULL; argc++);
@@ -97,7 +96,7 @@ void context_uload(PCB *_pcb, const char *filename, char* const argv[], char* co
 		for (int i = 0; i < argc; i++) {
 			for (int j = strlen(argv[i]) - 1; j >= 0; j--) {
 				*(char*)ustack_end = argv[i][j];
-				ustack_end--;
+				// ustack_end--;
 				// Log("ustack_end = %X", ustack_end);
 				// Log("%d", argv[i][j]); // printf %c in klib mal-implemented
 			}
